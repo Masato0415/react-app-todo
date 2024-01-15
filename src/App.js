a3468513a1d5be924c92ad34bc0587b269747958
Masato0415 <masakun1993@gmail.com>
2024-01-15 12:30:34 +0900

    ボタン、Todoの追加機能実装(仮)

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
 import React, { useState } from 'react';
-import { Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react';
+import { Menu, MenuButton, MenuList, MenuItem, Button, Checkbox, } from '@chakra-ui/react';
+import { v4 as uuidv4 } from 'uuid';
 //import { ChevronDownIcon } from '@chakra-ui/icons';
 
 function App() {
 
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = () => {
+
+    if (input === "") {
+      return;
+    }
+    const newTodos = [...todos];
+    newTodos.push({ id: uuidv4(), todo: input });
+    setTodos(newTodos);
+    setInput("");
+  }
+
+
   return (
     <>
-
       <header>
-        <p className="bg-lime-400 p-2 text-center text-slate-100">Todoアプリ(仮)</p>
+        <p>Todoアプリ(仮)</p>
       </header>
 
-      <div className="flex justify-center">
-        <input className="bg-green-50 border border-green-500  rounded-md w-[500px]" type="text" placeholder='Todoを入力してください(20文字以内)'></input>
-        <button className="bg-green-500 text-white   hover:bg-green-400 min-w-[50px]">入力</button>
+      <div id="inputForm">
+        <input value={input} onChange={(e) => { setInput(e.target.value) }}></input>
+        <button onClick={handleAddTodo}>Add</button>
       </div>
-      {/*リスト一覧画面*/}
-      <div id="todoList" className="bg-lime-400 min-h-[650px] m-[10px] rounded-lg p-[20px]">
-        <ul>
-          <li className="flex mb-[10px]">
-            <input id="checkbox" type="checkbox" className="w-5 h-5 cursor-pointer mr-3"></input>
-            <p className="text-1xl">カレーの食材を買う</p>
-          </li>
 
-          <li className="flex mb-[10px]">
-            <input id="checkbox" type="checkbox" className="w-5 h-5 cursor-pointer mr-3"></input>
-            <p className="text-1xl">カレーを作る</p>
+      <div>
 
-          </li>
-          <li className="flex mb-[10px] ">
-            <input id="checkbox" type="checkbox" className="w-5 h-5 cursor-pointer mr-3"></input>
-            <p className="text-1xl">カレーを食べる</p>
-          </li>
+        {todos.map((todo) => (
+          <div style={{ display: "flex" }}>
+            <Checkbox> </Checkbox>
+            <div key={todo.id}>{todo.todo}</div>
+          </div>
 
-        </ul>
+        ))}
+        
         <Menu>
           <MenuButton as={Button}>
             メニュー
@@ -41,18 +48,17 @@ function App() {
           <MenuList>
             <MenuItem>編集</MenuItem>
             <MenuItem>削除</MenuItem>
-            <MenuItem>終了</MenuItem>
           </MenuList>
         </Menu>
-
       </div>
-
-
     </>
 
+
+
+
   );
+
 
 }
 
 export default App;
-
