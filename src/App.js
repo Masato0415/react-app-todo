de69ea00ad88faae1c3d135923384ccae693dbba
Masato0415 <masakun1993@gmail.com>
2024-01-15 16:12:03 +0900

    todoの作成日,の追加

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Menu, MenuButton, MenuList, MenuItem, Button, Checkbox, } from '@chakra-ui/react';
+import { ChakraProvider, Menu, MenuButton, MenuList, MenuItem, Button, Checkbox, } from '@chakra-ui/react';
 import { v4 as uuidv4 } from 'uuid';
 //import { ChevronDownIcon } from '@chakra-ui/icons';
 
@@ -13,8 +13,23 @@ function App() {
     if (input === "") {
       return;
     }
+
+    function formatDate(date) {
+      const year = date.getFullYear();
+      console.log(year);
+      console.log(date.getMonth())
+      const month = ('0' + (date.getMonth() + 1)).slice(-2);
+      const day = ('0' + date.getDate()).slice(-2);
+      return `${year}-${month}-${day}`;
+    }
+
+    const today = new Date();
+
+    const formattedDate = formatDate(today);
+
+
     const newTodos = [...todos];
-    newTodos.push({ id: uuidv4(), todo: input });
+    newTodos.push({ id: uuidv4(), todo: input, checked: false, created: formattedDate });
     setTodos(newTodos);
     setInput("");
   }
@@ -22,35 +37,37 @@ function App() {
 
   return (
     <>
-      <header>
-        <p>Todoアプリ(仮)</p>
-      </header>
+      <ChakraProvider>
+        <header>
+          <p>Todoアプリ(仮)</p>
+        </header>
 
-      <div id="inputForm">
-        <input value={input} onChange={(e) => { setInput(e.target.value) }}></input>
-        <button onClick={handleAddTodo}>Add</button>
-      </div>
+        <div id="inputForm">
+          <input value={input} onChange={(e) => { setInput(e.target.value) }}></input>
+          <button onClick={handleAddTodo}>Add</button>
+        </div>
 
-      <div>
+        <div>
 
-        {todos.map((todo) => (
-          <div style={{ display: "flex" }}>
-            <Checkbox> </Checkbox>
-            <div key={todo.id}>{todo.todo}</div>
-          </div>
+          {todos.map((todo) => (
+            <div key={todo.id} style={{ display: "flex" }}>
+              <Checkbox></Checkbox>
+              <div>{todo.todo}</div>
+            </div>
 
-        ))}
-        
-        <Menu>
-          <MenuButton as={Button}>
-            メニュー
-          </MenuButton>
-          <MenuList>
-            <MenuItem>編集</MenuItem>
-            <MenuItem>削除</MenuItem>
-          </MenuList>
-        </Menu>
-      </div>
+          ))}
+
+          <Menu>
+            <MenuButton as={Button}>
+              メニュー
+            </MenuButton>
+            <MenuList>
+              <MenuItem>編集</MenuItem>
+              <MenuItem>削除</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </ChakraProvider>
     </>
 
 
